31c091c86a00825c0fb3feb34cb5feef4f5cb970
natanielshulkin <natysh1011@gmail.com>
2025-03-07 15:36:40 +0200

    add Question

---
+++ Exercises from the course website/Chapter 1 – Language Basics/1.4 Print command/Question 1.py
@@ -0,0 +1,63 @@
+"""
+ שאלה 1
+בהינתן הפקודה הבאה, בחרו מה יודפס:
+print ("Hello", "World")
+
+1.Hello World
+2.HelloWorld
+3.HelloWorld
+4.Hello, World
+"""
+print ("Hello", "World")
+
+"""
+שאלה 2
+בהינתן הפקודה הבאה, בחרו מה יודפס:
+print ("Lottery numbers are: ", "1", "26", "28", "31", "34", "36", end=".")
+Lottery numbers are:1 26 28 31 34 36
+Lottery numbers are:1 26 28 31 34 36.
+Lottery numbers are: 1 26 28 31 34 36. 
+Lottery numbers are: 1 26 28 31 34 36
+"""
+
+print ("Lottery numbers are: ", "1", "26", "28", "31", "34", "36", end=".")
+
+"""
+שאלה 3
+בהינתן הפקודה הבאה, בחרו מה יודפס:
+print ("P", "y", "t", "h", "o", "n", sep="\n")
+
+1.P y t h o n
+2.P
+  y
+  t
+  h
+  o
+  n
+3.Python
+4.P\ny\nt\nh\no\nn
+"""
+print ("P", "y", "t", "h", "o", "n", sep="\n")
+
+"""
+שאלה 4
+
+בהינתן הקוד הבא, בחרו מה יודפס:
+
+print ("1", "2", end="")
+print ("3", "4", sep=" ")
+print ("5", "6", end=".")
+
+a.1 2 3 4 5 6.
+b.1 2 3 4 5 6
+c.1 23 4 5 6.
+d.123456
+e.1 23 4
+  5 6. 
+
+"""
+
+print ("1", "2", end="")
+print ("3", "4", sep=" ")
+print ("5", "6", end=".")
+
---
+++ Exercises from the course website/Chapter 1 – Language Basics/1.4 Print command/Question 2.py
@@ -0,0 +1,13 @@
+# כלל השאלות כאן זה מה הוא מדפיס
+#שאלה 1
+first = 5
+second = 3
+print (first, "-", second, "=", first - second)
+#שאלה 2
+print ("Milk", "Bread", "Tomatoes", "Snacks", sep="#")
+#שאלה 3
+print ("E", "r", "r", "o", "r", "!", sep="")
+#שאלה 4
+balance = 3450 # סכום בחשבון
+deposit = 150 # הפקדה
+print ("Current account balance is", balance + deposit, "shekels.")
